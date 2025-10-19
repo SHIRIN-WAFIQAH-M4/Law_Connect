e961d1ec545a08f2be727000af0210142e2e4f75
Shirin Wafiqah <shirinwafiqah2@gmail.com>
2025-10-19 07:13:34 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,2 @@
+<script> window.chtlConfig = { chatbotId: "1556442627" } </script>
+<script async data-id="1556442627" id="chtl-script" type="text/javascript" src="https://chatling.ai/js/embed.js"></script>
